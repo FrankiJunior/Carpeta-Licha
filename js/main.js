2cf761fb1f9dedee9625c9d50c1a58b665bd2167
Lisandro Rezoagli <L>
2020-09-09 20:17:35 -0300

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
-let contador = 0;
+window.onload = function () {
+    let boton = document.getElementById("iconoID");
+    let enlaces = document.getElementById("enlaces");
+    let contador = 0;
+    boton.addEventListener("click", hola);
 
-boton.addEventListener("click", hola);
-
-function hola(){
-alert("Hola");
+    function hola() {
+        alert("Hola");
+    }
 }
