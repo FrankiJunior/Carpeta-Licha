ac8ed1547d614fd818082b398baa77e4dd55de88
FrankiJunior <64921331+FrankiJunior@users.noreply.github.com>
2020-09-09 12:26:17 -0300

    Add files via upload
    la la la

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
+
+boton.addEventListener("click", hola);
+
+function hola(){
+alert("Hola");
+}
